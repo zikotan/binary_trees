0bfb2ea3b88c91af9d237163647a12e7fdfe3af3
zikotan <tananzakaria630@gmail.com>
2024-01-31 18:31:35 +0100

    response

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - func
+ * @node: arg
+ *
+ * Return: Result
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - func
+ * @node: arg
+ *
+ * Return: Result
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
