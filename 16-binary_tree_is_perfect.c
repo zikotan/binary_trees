df40a1971f82da3224ec844b4057a73abb60c31f
zikotan <tananzakaria630@gmail.com>
2024-01-31 18:06:59 +0100

    response

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t myHeight = 0,myNodes = 0, pow = 0;
+	size_t myHeight = 0, myNodes = 0, pow = 0;
 
 	if (!tree)
 		return (0);
@@ -45,7 +45,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t heightLeft = 0,heightRight = 0;
+	size_t heightLeft = 0, heightRight = 0;
 
 	if (!tree)
 		return (0);
