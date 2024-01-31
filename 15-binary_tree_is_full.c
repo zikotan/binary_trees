9c27d8d051c9153c357e7b6639a643f84031ae46
zikotan <tananzakaria630@gmail.com>
2024-01-31 17:31:25 +0100

    response

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->right && !tree->left)
+		return (1);
+
+	if (tree->right && tree->left)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+
+	return (0);
+}
