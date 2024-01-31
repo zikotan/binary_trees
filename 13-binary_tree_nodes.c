1ccd06c7ba9c3120e176b4620253d26c35f4ac9b
zikotan <tananzakaria630@gmail.com>
2024-01-31 16:35:01 +0100

    response

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t myNodes = 0;
+
+	if (!tree)
+		return (0);
+
+	myNodes += binary_tree_nodes(tree->left);
+	myNodes += binary_tree_nodes(tree->right);
+
+	if (tree->left || tree->right)
+		myNodes += 1;
+
+	return (myNodes);
+}
