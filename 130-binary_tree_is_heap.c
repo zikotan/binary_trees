ceedd24899065da49078db997a1f49b4f0ffef5e
zikotan <tananzakaria630@gmail.com>
2024-02-01 20:28:43 +0100

    response

---
+++ 130-binary_tree_is_heap.c
@@ -0,0 +1,84 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_heap - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (assist6(tree));
+}
+
+/**
+ * binary_tree_is_complete - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+	return (assist7(tree, 0, size));
+}
+
+/**
+ * binary_tree_size - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
+
+/**
+ * assist6 - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+int assist6(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+	if (!binary_tree_is_complete(tree))
+		return (0);
+	if (tree->left)
+		if (tree->left->n > tree->n)
+			return (0);
+	if (tree->right)
+		if (tree->right->n > tree->n)
+			return (0);
+	return (assist6(tree->left) &&
+		assist6(tree->right));
+}
+
+/**
+ * assist7 - func
+ * @tree: arg1
+ * @ind: arg2
+ * @s: arg3
+ *
+ * Return: Result
+ */
+int assist7(const binary_tree_t *tree, size_t ind, size_t s)
+{
+	if (!tree)
+		return (1);
+	if (ind >= s)
+		return (0);
+	return (assist7(tree->left, 2 * ind + 1, s) &&
+		assist7(tree->right, 2 * ind + 2, s));
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -62,6 +62,7 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
 
 int powRec(int a, int b);
 void assist(const binary_tree_t *tree, void (*func)(int), size_t level);
