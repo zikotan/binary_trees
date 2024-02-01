de096159e41569ce8e9e6a6d65b3c88c9709a6ea
zikotan <tananzakaria630@gmail.com>
2024-02-01 18:33:08 +0100

    response

--- binary_trees.h
+++ binary_trees.h
@@ -57,11 +57,13 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 int powRec(int a, int b);
 void assist(const binary_tree_t *tree, void (*func)(int), size_t level);
 int otherAssist(const binary_tree_t *tree, size_t index, size_t size);
 int assist3(const binary_tree_t *tree, int min, int max);
 bst_t *bst_min_val(bst_t *root);
+int assist4(const binary_tree_t *tree, int min, int max);
 
 #endif
