647e148cbf6c1db4610a1971d0dd2f88544acd91
zikotan <tananzakaria630@gmail.com>
2024-02-01 16:17:58 +0100

    response

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - func
+ * @tree: arg
+ *
+ * Return: Result
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (assist3(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * assist3 - func
+ * @tree: arg1
+ * @min: arg2
+ * @max: arg3
+ *
+ * Return: Result
+ */
+int assist3(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (assist3(tree->left, min, tree->n - 1) &&
+		assist3(tree->right, tree->n + 1, max));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -51,9 +52,11 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 int powRec(int a, int b);
 void assist(const binary_tree_t *tree, void (*func)(int), size_t level);
 int otherAssist(const binary_tree_t *tree, size_t index, size_t size);
+int assist3(const binary_tree_t *tree, int min, int max);
 
 #endif
