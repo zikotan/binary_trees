9138866e51f1606f93e29819ce3f5dbd6f6bcb37
zikotan <tananzakaria630@gmail.com>
2024-02-02 02:24:34 +0100

    response

--- binary_trees.h
+++ binary_trees.h
@@ -66,6 +66,7 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 int powRec(int a, int b);
 void assist(const binary_tree_t *tree, void (*func)(int), size_t level);
