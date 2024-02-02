dffd5aad6dfd549130cd2151f0ff57f28ea0a3ad
zikotan <tananzakaria630@gmail.com>
2024-02-02 03:27:21 +0100

    response

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -9,5 +9,68 @@
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
+	bst_t *myNode = NULL, *myLeaf;
 
+	myLeaf = bst_search(root, value);
+	if (myLeaf == NULL)
+		return (root);
+	if (myLeaf->left != NULL && myLeaf->right == NULL)
+	{
+		for (myNode = myLeaf->left; myNode->right != NULL; myNode = myNode->right)
+			;
+		if (myNode != myLeaf->left)
+		{
+			myNode->parent->right = myNode->left;
+			myNode->left = myLeaf->left;
+		}
+		myNode->right = myLeaf->right, myNode->parent = myLeaf->parent;
+	}
+	else if (myLeaf->right != NULL)
+	{
+		for (myNode = myLeaf->right; myNode->left != NULL; myNode = myNode->left)
+			;
+		if (myNode != myLeaf->right)
+		{
+			myNode->parent->left = myNode->right;
+			myNode->right = myLeaf->right;
+		}
+		myNode->left = myLeaf->left, myNode->parent = myLeaf->parent;
+	}
+	if (myLeaf->parent != NULL && myLeaf->parent->right == myLeaf)
+		myLeaf->parent->right = myNode;
+	if (myLeaf->parent != NULL && myLeaf->parent->left == myLeaf)
+		myLeaf->parent->left = myNode;
+	free(myLeaf);
+	if (myNode->right != NULL && myNode != NULL)
+		myNode->right->parent = myNode;
+	if (myNode->left != NULL && myNode != NULL)
+		myNode->left->parent = myNode;
+	if (myNode->parent == NULL)
+		return (myNode);
+	return (root);
+}
+
+/**
+ * bst_search - func
+ * @tree: arg1
+ * @value: arg2
+ *
+ * Return: Result
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	bst_t *myNode = (bst_t *)tree;
+
+	if (!tree)
+		return (NULL);
+	while (myNode)
+	{
+		if (value < myNode->n)
+			myNode = myNode->left;
+		else if (value > myNode->n)
+			myNode = myNode->right;
+		else
+			return (myNode);
+	}
+	return (NULL);
 }
